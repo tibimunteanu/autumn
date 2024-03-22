52d2863bf4cf81833b05dd7f504ee66aa44c097c
tibimunteanu <sn_tibi@yahoo.com>
2024-03-22 20:53:00 +0200

    cleanup

--- lua/autumn/config.lua
+++ lua/autumn/config.lua
@@ -1,25 +1,11 @@
 local M = {}
 
 ---@class Config
----@field on_colors fun(colors: ColorScheme)
----@field on_highlights fun(highlights: Highlights, colors: ColorScheme)
 local defaults = {
   transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
 }
 
 ---@type Config
--- lua/autumn/util.lua
+++ lua/autumn/util.lua
@@ -12,7 +12,7 @@ end
 ---@param foreground string foreground color
 ---@param background string background color
 ---@param alpha number|string number between 0 and 1. 0 results in bg, 1 results in fg
-function M.blend(foreground, background, alpha)
+local function M.blend(foreground, background, alpha)
   alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
   local bg = hexToRgb(background)
   local fg = hexToRgb(foreground)
@@ -31,19 +31,6 @@ end
 
 function M.lighten(hex, amount, fg)
   return M.blend(hex, fg or M.fg, amount)
-end
-
-function M.invert_color(color)
-  local hsluv = require("autumn.hsluv")
-  if color ~= "NONE" then
-    local hsl = hsluv.hex_to_hsluv(color)
-    hsl[3] = 100 - hsl[3]
-    if hsl[3] < 40 then
-      hsl[3] = hsl[3] + (100 - hsl[3])
-    end
-    return hsluv.hsluv_to_hex(hsl)
-  end
-  return color
 end
 
 ---@param group string
@@ -95,20 +82,6 @@ function M.autocmds(config)
   end
 end
 
--- Simple string interpolation.
---
--- Example template: "${name} is ${value}"
---
----@param str string template string
----@param table table key value pairs to replace in the string
-function M.template(str, table)
-  return (
-    str:gsub("($%b{})", function(w)
-      return vim.tbl_get(table, unpack(vim.split(w:sub(3, -2), ".", { plain = true }))) or w
-    end)
-  )
-end
-
 function M.syntax(syntax)
   for group, colors in pairs(syntax) do
     M.highlight(group, colors)
@@ -145,33 +118,6 @@ function M.terminal(colors)
   vim.g.terminal_color_14 = colors.cyan
 end
 
----@param colors ColorScheme
-function M.invert_colors(colors)
-  if type(colors) == "string" then
-    ---@diagnostic disable-next-line: return-type-mismatch
-    return M.invert_color(colors)
-  end
-  for key, value in pairs(colors) do
-    colors[key] = M.invert_colors(value)
-  end
-  return colors
-end
-
----@param hls Highlights
-function M.invert_highlights(hls)
-  for _, hl in pairs(hls) do
-    if hl.fg then
-      hl.fg = M.invert_color(hl.fg)
-    end
-    if hl.bg then
-      hl.bg = M.invert_color(hl.bg)
-    end
-    if hl.sp then
-      hl.sp = M.invert_color(hl.sp)
-    end
-  end
-end
-
 ---@param theme Theme
 function M.load(theme)
   -- only needed to clear when not the default colorscheme
@@ -184,10 +130,7 @@ function M.load(theme)
 
   M.syntax(theme.highlights)
 
-  -- vim.api.nvim_set_hl_ns(M.ns)
-  if theme.config.terminal_colors then
-    M.terminal(theme.colors)
-  end
+  M.terminal(theme.colors)
 
   M.autocmds(theme.config)
 
