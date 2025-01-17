462163b469884bdf176c8456ef07d19051c8762e
Tibi Munteanu <sn_tibi@yahoo.com>
2025-01-17 21:53:07 +0200

    transparent bg

--- lua/autumn/config.lua
+++ lua/autumn/config.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local defaults = {
-  transparent = false, -- Enable this to disable setting the background color
+  transparent = true, -- Enable this to disable setting the background color
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
